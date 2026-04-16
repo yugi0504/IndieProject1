a1c5621b2778d70434a34b8a38c52830de6964a0
mafuyu <yugi1978seiko@icloud.com>
2026-04-16 19:33:14 +0900

    Player class完成、enemy class追加

---
+++ IndieProject1/IndieProject1/Enemy.cpp
@@ -0,0 +1,53 @@
+#include "Enemy.hpp"
+
+void Enemy::Initialize(int modelHandle)
+{
+	m_modelHandle = modelHandle;
+
+	m_bodyCollider = make_shared<CapsuleCollider>(1.0f, 0.5f);
+
+	AddCollider(m_bodyCollider);
+
+	m_status.hp = 300;
+	m_status.maxHp = 300;
+
+}
+
+void Enemy::OnUpdate(float demtaTime)
+{
+	
+
+	if (input::KeyTrigger(KEY_INPUT_F2))
+	{
+		m_drawColliderDebug = !m_drawColliderDebug;
+	}
+
+}
+
+void Enemy::OnDraw()
+{
+	if (m_modelHandle >= 0)
+	{
+		MV1SetPosition(m_modelHandle, m_transform.position);
+		MV1SetRotationXYZ(m_modelHandle, m_transform.rotate);
+		MV1DrawModel(m_modelHandle);
+	}
+
+	if (m_drawColliderDebug && m_bodyCollider)
+	{
+		m_bodyCollider->Draw();
+	}
+}
+
+void Enemy::OnDamaged(const DamageInfo& dmg)
+{
+
+}
+
+void Enemy::OnDead()
+{
+
+}
+
+
+
---
+++ IndieProject1/IndieProject1/Enemy.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include <memory>
+
+#include "Character.hpp"
+#include "CapsuleCollider.hpp"
+#include "Damage.hpp"
+
+#include "Input.hpp"
+
+class Enemy final : public Character
+{
+	// ---------------------------
+	// ƒƒ“ƒo[•Ï”
+	// ---------------------------
+
+private:
+	int m_modelHandle = -1;
+	shared_ptr<CapsuleCollider> m_bodyCollider;
+	bool m_drawColliderDebug = false;
+
+	// ---------------------------
+	// ŠÖ”
+	// ---------------------------
+
+public:
+	Enemy() = default;
+	~Enemy() override = default;
+
+	void Initialize(int modelHandle);
+
+	shared_ptr<CapsuleCollider> GetHurtCollider() const noexcept;
+
+protected:
+	void OnUpdate(float deltaTime) override;
+	void OnDraw() override;
+	void OnDamaged(const DamageInfo& dmg) override;
+	void OnDead() override;
+};
